9798337993ef634d4275740fb31e408840e5d29d
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2025-04-06 11:59:07 +0200

    Learning Vue Basics:
    - Declarative Rendering
    
    - Attribute Bindings

---
+++ OvningEtt/app.js
@@ -0,0 +1,11 @@
+// const { app } = Vue.createApp({});
+
+// app.component('BlogPost', {
+//   template: `<div>
+//   <h1>{{someTitle}}</h1>
+//   </div>`,
+//   data(){
+//   return {
+//   someTitle: 'My first Post'
+//   }
+// });
